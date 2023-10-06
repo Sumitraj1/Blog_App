9347ae4ba82838ac7291b4e3a868c7772b9ada9f
sumitraj1-Git <rajsumitpandya6.com>
2023-10-06 23:44:28 +0530

    first commit

---
+++ users/admin.py
@@ -0,0 +1,9 @@
+# django_project/users/admin.py
+from django.contrib import admin
+from .models import CustomUser, SubscribedUsers
+
+class SubscribedUsersAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'created_date')
+
+admin.site.register(CustomUser)
+admin.site.register(SubscribedUsers, SubscribedUsersAdmin)
---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.utils import timezone
+import os
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    def image_upload_to(self, instance=None):
+        if instance:
+            return os.path.join('Users', self.username, instance)
+        return None
+
+    STATUS = (
+        ('regular', 'regular'),
+        ('subscriber', 'subscriber'),
+        ('moderator', 'moderator'),
+    )
+
+    email = models.EmailField(unique=True)
+    status = models.CharField(max_length=100, choices=STATUS, default='regular')
+    description = models.TextField("Description", max_length=600, default='', blank=True)
+    image = models.ImageField(default='default/download.jpeg', upload_to=image_upload_to)
+
+    def __str__(self):
+        return self.username
+    
+class SubscribedUsers(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True, max_length=100)
+    created_date = models.DateTimeField('Date created', default=timezone.now)
+
+    def __str__(self):
+        return self.email    
